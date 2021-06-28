ece102754e0347b8a12d98d5976f837557c721f7
Ebube111 <46991166+Ebube111@users.noreply.github.com>
2021-06-28 13:09:16 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,10 @@
-#include
-
-void reset_to_98(int *n){
-  *n = 98;
+#include "holberton.h"
+/**
+* reset_to_98 -a function that takes a pointer to an int
+* @n: puntero
+* Return: (0)
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
