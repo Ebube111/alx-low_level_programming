e72d27f31939e95f4656f5170a38ec324ade925c
Ebube111 <46991166+Ebube111@users.noreply.github.com>
2021-07-09 20:25:07 +0100

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,1 +1,18 @@
-#include
+#include<stdio.h>
+#include "holberton.h"
+/**
+*_strlen - returns the length of a string..
+*@s: value to be evaluate.
+*Return: not.
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
