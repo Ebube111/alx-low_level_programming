c45ff1035791005f1331afd3ff0740ebf4daa7b9
Ebube111 <46991166+Ebube111@users.noreply.github.com>
2021-07-09 20:37:38 +0100

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,1 +1,21 @@
-#include
+#include "holberton.h"
+
+/**
+* _abs - gives the absolute value
+* of an integer.
+*
+* @n: input number as an integer.
+*
+* Return: absolute value
+*/
+int _abs(int n)
+	{
+	if (n >= 0)
+	{
+	return (n);
+	}
+	else
+	{
+	return (n * -1);
+	}
+}
