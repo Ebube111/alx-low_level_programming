dbea5550b43f75f53527a2383f7de926e1eee6ce
Ebube111 <46991166+Ebube111@users.noreply.github.com>
2021-07-29 17:32:44 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,23 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
-
+#include <stddef.h>
+#include <stdlib.h>
 /**
- * list_len - returns the number of elements in a linked_list
- * @h: pointer to the struct linked_list
- * Return: number of elements
- **/
-
+ * list_len - finds the number of nodes in a list
+ * @h: singly linked list
+ * Return: number of elements in a linked list
+ */
 size_t list_len(const list_t *h)
 {
-  size_t elements;
+	int i;
 
-  elements = 0;
-  while (h)
-    {
-      h = h->next;
-      elements += 1;
-    }
-
-  return (elements);
+	for (i = 0; h; i++)
+		h = h->next;
+	return (i);
 }
