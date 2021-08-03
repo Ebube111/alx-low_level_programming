f9d0a74705e56ef25ef1d63f77db77679b3a48cb
Ebube111 <46991166+Ebube111@users.noreply.github.com>
2021-08-03 15:01:30 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,25 +4,25 @@
 #include "lists.h"
 
 /**
- * print_listint - a func that prints all the elements of a list
- * @h: holds the head to the linked list
- * Return: the number of nodes
- **/
+* print_listint - a func that prints all the elements of a list
+* @h: holds the head to the linked list
+* Return: the number of nodes
+**/
 
 size_t print_listint(const listint_t *h)
 {
-  size_t node_C;
-  const listint_t *next_ptr;
+	size_t node_C;
+	const listint_t *next_ptr;
 
-  next_ptr = h;
-  node_C = 0;
+	next_ptr = h;
+	node_C = 0;
 
-  while (next_ptr != NULL)
-    {
-      printf("%i\n", next_ptr->n);
-      node_C += 1;
-      next_ptr = next_ptr->next;
-    }
+	while (next_ptr != NULL)
+	{
+		printf("%i\n", next_ptr->n);
+		node_C += 1;
+		next_ptr = next_ptr->next;
+	}
 
-  return (node_C);
+	return (node_C);
 }
