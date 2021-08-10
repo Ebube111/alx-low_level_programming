275352eb39033472da3d4e35b36c77aeb1a1b1a6
Ebube111 <46991166+Ebube111@users.noreply.github.com>
2021-08-10 04:03:21 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,41 @@
-#include "holberton.h"
 #include "main.h"
-
+#include "holberton.h"
 /**
- * create_file - a function that creates a file
- * @filename: the filename
- * @text_content: a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure
- **/
-
+ * create_file - function that creates a file
+ * permission rw-------
+ * If the file already exists, do not change the permissions.
+ * if the file already exists, truncate it
+ *
+ * @filename: name of the file to create
+ * if filename is NULL return -1
+ * @text_content: NULL terminated string to write to the file
+ * if text_content is NULL create an empty file
+ *
+ * Return: 1 on succes
+ * -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…)
+ */
 int create_file(const char *filename, char *text_content)
 {
-  int f;
-  int length, res;
+	int fd, num_written, len = 0;
 
-  if (filename == NULL)
-    return (-1);
-  f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-  if (f == -1)
-    return (-1);
+	if (filename == 0)
+		return (-1);
 
-  if (!text_content)
-    return (1);
-  length = 0;
-  while (text_content[length] != '\0')
-    length += 1;
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
-  res = write(f, text_content, length);
-  if (res != length)
-    return (-1);
-  res = close(f);
-  if (res == -1)
-    exit(-1);
+	if (fd == -1)
+		return (-1);
 
-  return (1);
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+		num_written = write(fd, text_content, len);
+
+		if (num_written == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
 }
