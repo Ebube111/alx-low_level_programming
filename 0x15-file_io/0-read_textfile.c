64f12167d1681f2c20101ccdcd415904bd8f73a8
--global <--global>
2021-08-09 19:44:40 -0700

    create file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,37 @@
 #include "main.h"
-
+#include "holberton.h"
 /**
- * read_textfile - a function that reads a text file
- *and prints it to the POSIX standard output
- * @filename: the file name
- * @letters: the number of letters
- * Return: the actual number of letters or 0 if fails
- **/
-
+ * read_textfile - reads a text file and prints it
+ * to the POSIX standard output
+ * @filename: name of the file
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int f, length, i, res;
+  int fd, num_to_print, written_num;
   char *buffer;
 
-  if (filename == NULL)
-    return (0);
-  /* open */
-
-  f = open(filename, O_RDONLY);
-
-  if (f == -1)
+  if (!filename)
     return (0);
 
-  buffer = malloc(sizeof(char) * letters);
+  fd = open(filename, O_RDONLY);
+  if (fd == -1)
+    return (0);
+
+  buffer = malloc(letters * sizeof(char));
   if (!buffer)
     return (0);
 
-  read(f, buffer, letters);
-  buffer[letters] = '\0';
+  /* return Number of bytes read on success */
+  num_to_print = read(fd, buffer, letters);
+  /* return Number of bytes written on success */
+  written_num = write(STDOUT_FILENO, buffer, num_to_print);
 
-  for (i = 0; buffer[i] != '\0'; i += 1)
-    length += 1;
-
-  res = close(f);
-  if (res != 0)
-    exit(-1);
-  res = write(STDOUT_FILENO, buffer, length);
-  if (res != length)
+  if (written_num < 0)
     return (0);
-  free(buffer);
 
-  return (length);
+  free(buffer);
+  close(fd);
+  return (num_to_print);
 }
