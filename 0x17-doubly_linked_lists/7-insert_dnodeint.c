0ebc694aff2e04009ccf474aef1f363a6786d6cf
Ebube111 <ebubeagwaze@gmail.com>
2021-09-17 08:10:38 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,41 +1,54 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
- *                            list at a given position.
- * @h: A pointer to the head of the dlistint_t list.
- * @idx: The position to insert the new node.
- * @n: The integer for the new node to contain.
- *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new node.
- */
+* insert_dnodeint_at_index - a function that inserts a
+*new node at a given position
+* @h: the pointer to a pointer to a linked list
+* @idx: is the index of the list where the new node
+*should be added. Index starts at 0
+* @n: the data in the list
+* Return: the address of the new node, or NULL if it failed
+*/
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-  dlistint_t *tmp = *h, *new;
+	dlistint_t *ptr, *new, *previous = NULL;
+	unsigned int i = 0;
 
-  if (idx == 0)
-    return (add_dnodeint(h, n));
-
-  for (; idx != 1; idx--)
-    {
-      tmp = tmp->next;
-      if (tmp == NULL)
-	return (NULL);
-    }
-
-  if (tmp->next == NULL)
-    return (add_dnodeint_end(h, n));
-
-  new = malloc(sizeof(dlistint_t));
-  if (new == NULL)
-    return (NULL);
-
-  new->n = n;
-  new->prev = tmp;
-  new->next = tmp->next;
-  tmp->next->prev = new;
-  tmp->next = new;
-
-  return (new);
+	if (h == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	if (*h == NULL && idx == 0)
+	{
+		new->next = NULL;
+		new->prev = NULL;
+		*h = new;
+		return (new);
+	}
+	ptr = *h;
+	while (i < idx && ptr != NULL)
+	{
+		i += 1;
+		previous = ptr;
+		ptr = ptr->next;
+	}
+	if (i != idx)
+		return (NULL);
+	if (idx == 0)
+	{
+		new->next = *h;
+		new->prev = NULL;
+		(*h)->prev = new;
+		*h = new;
+		return (new);
+	}
+	if (ptr != NULL)
+		ptr->prev = new;
+	new->next = ptr;
+	new->prev = previous;
+	previous->next = new;
+	return (new);
 }
