a7b5bc8de2ce0536382355cf0000c7b62279da9f
Ebube111 <ebubeagwaze@gmail.com>
2021-09-17 08:09:09 +0100

    Update 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,21 +1,31 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - Locates a node in a dlistint_t list.
- * @head: The head of the dlistint_t list.
- * @index: The node to locate.
- *
- * Return: If the node does not exist - NULL.
- *         Otherwise - the address of the located node.
- */
+* get_dnodeint_at_index - a function that returns the nth
+*node of a dlistint_t linked list.
+* @head: pointer that point to a linked list
+* @index: is the index of the node, starting from 0
+* Return: the node or NULL if the node does not exist
+**/
+
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-  for (; index != 0; index--)
-    {
-      if (head == NULL)
-	return (NULL);
-      head = head->next;
-    }
+	dlistint_t *ptr;
+	unsigned int i = 0;
 
-  return (head);
+	if (!head)
+		return (NULL);
+
+	ptr = head;
+
+	while (i < index && ptr != NULL)
+	{
+		ptr = ptr->next;
+		i += 1;
+	}
+
+	if (i != index)
+		return (NULL);
+
+	return (ptr);
 }
