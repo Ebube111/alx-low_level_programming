3c49d3a50bb9c759746f1e855f4a5bd796516955
--global <--global>
2021-09-20 13:54:04 -0700

    dynamics

--- 0x18-dynamic_libraries/2-strlen.c
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -1,23 +1,16 @@
 #include "main.h"
 
 /**
- **_strchr - Return a pointer.
- * @s: s
- * @c: c
+ * _strlen - return the length of a string.
+ *@s: s
  * Return: Always 0.
  */
-char *_strchr(char *s, char c)
+int _strlen(char *s)
 {
   int i;
 
-  for (i = 0; s[i] >= '\0'; i++)
+  for (i = 0; s[i] != '\0'; i++)
     {
-      if (s[i] == c)
-	{
-	  return (s + i);
-	}
     }
-  {
-    return ('\0');
-  }
+  return (i);
 }
