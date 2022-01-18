ca7def1e79a1d68aaf1ae9e917771a39ada9e3b3
--global <--global>
2022-01-17 23:15:31 -0800

    first commit

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef HEADER
+#define HEADER
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void prar(int *array, size_t start, size_t end);
+#endif
